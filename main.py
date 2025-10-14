94b6ed30b80573ef96b0c7051b676f404e62817d
biryani79house-debug <biryani79house@gmail.com>
2025-10-14 12:51:06 +0530

    modified:   index.html 	modified:   main.py

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ class ProductStockLedger(BaseModel):
     total_sales: int
     history: List[ProductStockHistory]
 
-class ProductStockSnapshot(BaseModel):
+iclass ProductStockSnapshot(BaseModel):
     product_id: int
     product_name: str
     price: float
